38f4d3a05acda1c63e9a0bf0a398165aacd75a90
Jean-Christophe Vassort <jean-christophe@passbolt.com>
2022-03-18 11:19:28 +0100

    Add example.py

---
+++ example.py
@@ -0,0 +1,30 @@
+from passbolt import PassboltAPI
+from pprint import pprint
+import json
+
+p = PassboltAPI()
+
+print()
+print("Search for resource 64107118-bc0e-40ff-ae19-2b60c7516e19 (Supabase)")
+print("------")
+pprint(p.get_resource_per_uuid("64107118-bc0e-40ff-ae19-2b60c7516e19"))
+print()
+
+print("Search for the first resource who match the name 'Snyk'")
+print("------")
+
+resource = next(item for item in p.get_resources() if item["name"] == "Snyk")
+pprint(resource)
+
+# Descrypt Snyk secrets
+res = json.loads(p.decrypt(p.get_resource_secret(resource["id"])).data)
+
+print()
+print("Display Snyk password")
+print("------")
+print(res["password"])
+print()
+print("Display Snyk description")
+print("------")
+print(res["description"])
+print()
