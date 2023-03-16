f8390fad54c3af664754e5c0241fb71f93c09e10
Jean-Christophe Vassort <anatomicjc@open-web.fr>
2023-03-16 21:19:50 +0100

    Move to pyproject.toml / setup.cfg

--- setup.py
+++ setup.py
@@ -2,54 +2,25 @@
 # https://github.com/pypa/twine
 #
 # Upgrade twine
-#     python3 -m pip install --user --upgrade twine
+#     python -m pip install --upgrade twine
 #
 # Run this to build the `dist/PACKAGE_NAME-xxx.tar.gz` file
-#     rm -rf ./dist && python3 setup.py sdist
+#     rm -rf ./dist && python -m build
 #
 # Check dist/*
-#     python3 -m twine check dist/*
+#     python -m twine check dist/*
 #
 # Run this to build & upload it to `pypi`, type your account name when prompted.
-#     python3 -m twine upload dist/*
+#     python -m twine upload dist/*
 #
 # In one command line:
 #     rm -rf ./dist && python3 setup.py sdist bdist_wheel && python3 -m twine check dist/*
 #     rm -rf ./dist && python3 setup.py sdist bdist_wheel && python3 -m twine upload dist/*
 #
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Usage: python setup.py sdist bdist_wheel
 
-links = []  # for repo urls (dependency_links)
-
-with open("requirements.txt") as fp:
-    install_requires = fp.read()
-
-DESCRIPTION = "Python library for Passbolt, an open-source manager for teams"
-VERSION = "0.0.14"
-
-with open("README.md", "r") as fh:
-    LONG_DESCRIPTION = fh.read()
-
-setup(
-    name="py-passbolt",
-    version=VERSION,
-    author="Jean-Christophe Vassort",
-    author_email="anatomicjc@open-web.fr",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/passbolt/lab-passbolt-py",
-    license="MIT-0",
-    packages=find_packages(),
-    platforms=["any"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=install_requires,
-    dependency_links=links,
-    include_package_data=True,
-)
+if __name__ == "__main__":
+    setup()
